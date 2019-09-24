27b977c0f1533b190e4e91adaa9fee4f0d3046e2
Phrancis7 <50919685+Phrancis7@users.noreply.github.com>
2019-09-23 22:47:23 -0700

    Mars
    updated assignment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Use flask_pymongo to set up mongo connection
+# using flask_pymongo to set up mongo connection
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
